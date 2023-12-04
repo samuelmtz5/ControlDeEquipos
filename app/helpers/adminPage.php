d45cae5f2684f6e342bcf7f5f7f376f6bff22dec
Samuel Magaña <samuelmartinez2122@gmail.com>
2023-12-04 14:41:51 -0600

    Inicio del Login

---
+++ app/helpers/adminPage.php
@@ -0,0 +1,240 @@
+<?php
+/*
+*	Clase para definir las plantillas de las páginas web del sitio privado.
+*/
+class Dashboard_Page
+{
+    /*
+    *   Método para imprimir la plantilla del encabezado.
+    *
+    *   Parámetros: $title (título de la página web y del contenido principal).
+    *
+    *   Retorno: ninguno.
+    */
+    public static function headerTemplate($title)
+    {
+        // Se crea una sesión o se reanuda la actual para poder utilizar variables de sesión en las páginas web.
+        session_start();
+        // Se imprime el código HTML de la cabecera del documento.
+        print('
+            <!DOCTYPE html>
+            <html lang="es">
+                <head>
+                    <meta charset="utf-8">
+                    <title>Dashboard - ' . $title . '</title>
+                    <link type="image/png" rel="icon" href="../../resources/img/pc.png"/>
+                    <link type="text/css" rel="stylesheet" href="../../resources/css/materialize.min.css"/>
+                    <link type="text/css" rel="stylesheet" href="../../resources/css/material_icons.css"/>
+                    <link type="text/css" rel="stylesheet" href="../../resources/css/dashboard.css"/>
+                    <link type="text/css" rel="stylesheet" href="../../resources/css/vanilla-dataTables.min.css"/>
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+                </head>
+                <body>
+        ');
+        // Se obtiene el nombre del archivo de la página web actual.
+        $filename = basename($_SERVER['PHP_SELF']);
+        // Se comprueba si existe una sesión de administrador para mostrar el menú de opciones, de lo contrario se muestra un menú vacío.
+        if (isset($_SESSION['idUsuario'])) {
+            // Se verifica si la página web actual es diferente a index.php (Iniciar sesión) y a register.php (Crear primer usuario) para no iniciar sesión otra vez, de lo contrario se direcciona a main.php
+            if ($filename != 'index.php' && $filename != 'register.php') {
+                // Se llama al método que contiene el código de las cajas de dialogo (modals).
+                self::modals();
+                // Se imprime el código HTML para el encabezado del documento con el menú de opciones.
+                print('
+                    <header>
+                        <div class="navbar-fixed">
+                            <nav class="light-blue darken-1">
+                                <div class="nav-wrapper">
+                                    <a href="main.php" class="brand-logo"><img src="../../resources/img/FPA_Blanco.png" height="60"></a>
+                                    <a href="#" data-target="mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+                                    <ul class="right hide-on-med-and-down">
+                                        <li><a href="productos.php"><i class="material-icons left">shop</i>Productos</a></li>
+                                        <li><a href="categorias.php"><i class="material-icons left">shop_two</i>Categorías</a></li>
+                                        <li><a href="clientes.php"><i class="material-icons left">contacts</i>Clientes</a></li>
+                                        <li><a href="usuarios.php"><i class="material-icons left">group</i>Usuarios</a></li>
+                                        <li><a href="#" class="dropdown-trigger" data-target="dropdown"><i class="material-icons left">verified_user</i>Cuenta: <b>' . $_SESSION['username'] . '</b></a></li>
+                                    </ul>
+                                    <ul id="dropdown" class="dropdown-content">
+                                        <li><a href="#" onclick="openProfileDialog()"><i class="material-icons">face</i>Editar perfil</a></li>
+                                        <li><a href="#" onclick="openPasswordDialog()"><i class="material-icons">lock</i>Cambiar clave</a></li>
+                                        <li><a href="#" onclick="logOut()"><i class="material-icons">clear</i>Salir</a></li>
+                                    </ul>
+                                </div>
+                            </nav>
+                        </div>
+                        <ul class="sidenav" id="mobile">
+                            <li><a><i class="material-icons"></i></a></li>
+                            <a href="main.php" class="brand-logo"><img src="../../resources/img/FPA.png" weight="50" height="50"></a>
+                            <li><a><i class=""></i></a></li>
+                            <li><a href="productos.php"><i class="material-icons">shop</i>Productos</a></li>
+                            <li><a href="categorias.php"><i class="material-icons">shop_two</i>Categorías</a></li>
+                            <li><a href="clientes.php"><i class="material-icons">contacts</i>Clientes</a></li>
+                            <li><a href="usuarios.php"><i class="material-icons">group</i>Usuarios</a></li>
+                            <li><a class="dropdown-trigger" href="#" data-target="dropdown-mobile"><i class="material-icons">verified_user</i>Cuenta: <b>' . $_SESSION['username'] . '</b></a></li>
+                        </ul>
+                        <ul id="dropdown-mobile" class="dropdown-content">
+                            <li><a href="#" onclick="openProfileDialog()">Editar perfil</a></li>
+                            <li><a href="#" onclick="openPasswordDialog()">Cambiar clave</a></li>
+                            <li><a href="#" onclick="logOut()">Salir</a></li>
+                        </ul>
+                    </header>
+                    <main class="container">
+                        <h3 class="center-align">' . $title . '</h3>
+                ');
+            } else {
+                header('location: main.php');
+            }
+        } else {
+            // Se verifica si la página web actual es diferente a index.php (Iniciar sesión) y a register.php (Crear primer usuario) para direccionar a index.php, de lo contrario se muestra un menú vacío.
+            if ($filename != 'index.php' && $filename != 'register.php') {
+                header('location: index.php');
+            } else {
+                // Se imprime el código HTML para el encabezado del documento con un menú vacío cuando sea iniciar sesión o registrar el primer usuario.
+                print('
+                    <header>
+                        <div class="navbar-fixed">
+                            <nav class="blue darken-1">
+                                <div class="nav-wrapper">
+                                    <a href="index.php" class="brand-logo"><i class="material-icons">Sitio de Administración</i></a>
+                                </div>
+                            </nav>
+                        </div>
+                    </header>
+                    <main class="container">
+                        <h3 class="center-align">' . $title . '</h3>
+                ');
+            }
+        }
+    }
+
+    /*
+    *   Método para imprimir la plantilla del pie.
+    *
+    *   Parámetros: $controller (nombre del archivo que sirve como controlador de la página web).
+    *
+    *   Retorno: ninguno.
+    */
+    public static function footerTemplate($controller)
+    {
+        // Se comprueba si existe una sesión de administrador para imprimir el pie respectivo del documento.
+        if (isset($_SESSION['idUsuario'])) {
+            $scripts = '
+                <script type="text/javascript" src="../../resources/js/materialize.min.js"></script>
+                <script type="text/javascript" src="../../resources/js/sweetalert.min.js"></script>
+                <script type="text/javascript" src="../../resources/js/vanilla-dataTables.min.js"></script>
+                <script type="text/javascript" src="../../app/helpers/components.js"></script>
+                <script type="text/javascript" src="../../app/controllers/dashboard/initialization.js"></script>
+                <script type="text/javascript" src="../../app/controllers/dashboard/account.js"></script>
+                <script type="text/javascript" src="../../app/controllers/dashboard/' . $controller . '"></script>
+            ';
+        } else {
+            $scripts = '
+                <script type="text/javascript" src="../../resources/js/materialize.min.js"></script>
+                <script type="text/javascript" src="../../resources/js/sweetalert.min.js"></script>
+                <script type="text/javascript" src="../../app/helpers/components.js"></script>
+                <script type="text/javascript" src="../../app/controllers/dashboard/' . $controller . '"></script>
+            ';
+        }
+        print('
+                    </main>
+                    <footer class="page-footer blue darken-1">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col s12 m6 l6">
+                                    <h5 class="white-text">Dashboard</h5>
+                                </div>
+                            
+                            </div>
+                        </div>
+                        <div class="footer-copyright">
+                            <div class="container">
+                                <span>©Fundacion Padre Arrupe, todos los derechos reservados.</span>
+                                
+                            </div>
+                        </div>
+                    </footer>
+                    ' . $scripts . '
+                </body>
+            </html>
+        ');
+    }
+
+    /*
+    *   Método para imprimir las cajas de dialogo (modals) de editar pefil y cambiar contraseña.
+    */
+    private static function modals()
+    {
+        // Se imprime el código HTML de las cajas de dialogo (modals).
+        print('
+            <!-- Componente Modal para mostrar el formulario de editar perfil -->
+            <div id="profile-modal" class="modal">
+                <div class="modal-content">
+                    <h4 class="center-align">Editar perfil</h4>
+                    <form method="post" id="profile-form">
+                        <div class="row">
+                            <div class="input-field col s12 m6">
+                                <i class="material-icons prefix">person</i>
+                                <input id="nombres_perfil" type="text" name="nombres_perfil" class="validate" required/>
+                                <label for="nombres_perfil">Nombres</label>
+                            </div>
+                            <div class="input-field col s12 m6">
+                                <i class="material-icons prefix">person</i>
+                                <input id="apellidos_perfil" type="text" name="apellidos_perfil" class="validate" required/>
+                                <label for="apellidos_perfil">Apellidos</label>
+                            </div>
+                            <div class="input-field col s12 m6">
+                                <i class="material-icons prefix">email</i>
+                                <input id="correo_perfil" type="email" name="correo_perfil" class="validate" required/>
+                                <label for="correo_perfil">Correo</label>
+                            </div>
+                            <div class="input-field col s12 m6">
+                                <i class="material-icons prefix">person_pin</i>
+                                <input id="alias_perfil" type="text" name="alias_perfil" class="validate" required/>
+                                <label for="alias_perfil">Alias</label>
+                            </div>
+                        </div>
+                        <div class="row center-align">
+                            <a href="#" class="btn waves-effect grey tooltipped modal-close" data-tooltip="Cancelar"><i class="material-icons">cancel</i></a>
+                            <button type="submit" class="btn waves-effect blue tooltipped" data-tooltip="Guardar"><i class="material-icons">save</i></button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+            <!-- Componente Modal para mostrar el formulario de cambiar contraseña -->
+            <div id="password-modal" class="modal">
+                <div class="modal-content">
+                    <h4 class="center-align">Cambiar contraseña</h4>
+                    <form method="post" id="password-form">
+                        <div class="row">
+                            <div class="input-field col s12 m6 offset-m3">
+                                <i class="material-icons prefix">security</i>
+                                <input id="clave_actual" type="password" name="clave_actual" class="validate" required/>
+                                <label for="clave_actual">Clave actual</label>
+                            </div>
+                        </div>
+                        <div class="row center-align">
+                            <label>CLAVE NUEVA</label>
+                        </div>
+                        <div class="row">
+                            <div class="input-field col s12 m6">
+                                <i class="material-icons prefix">security</i>
+                                <input id="clave_nueva_1" type="password" name="clave_nueva_1" class="validate" required/>
+                                <label for="clave_nueva_1">Clave</label>
+                            </div>
+                            <div class="input-field col s12 m6">
+                                <i class="material-icons prefix">security</i>
+                                <input id="clave_nueva_2" type="password" name="clave_nueva_2" class="validate" required/>
+                                <label for="clave_nueva_2">Confirmar clave</label>
+                            </div>
+                        </div>
+                        <div class="row center-align">
+                            <a href="#" class="btn waves-effect grey tooltipped modal-close" data-tooltip="Cancelar"><i class="material-icons">cancel</i></a>
+                            <button type="submit" class="btn waves-effect blue tooltipped" data-tooltip="Guardar"><i class="material-icons">save</i></button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        ');
+    }
+}
---
+++ views/admin/main.php
@@ -0,0 +1,22 @@
+<?php
+// Se incluye la clase con las plantillas del documento.
+require_once('../../app/helpers/dashboard_page.php');
+// Se imprime la plantilla del encabezado enviando el título de la página web.
+Dashboard_Page::headerTemplate('Bienvenido');
+?>
+
+<!-- Se muestra un saludo de acuerdo con la hora del cliente -->
+<div class="row">
+    <h4 class="center-align blue-text" id="greeting"></h4>
+</div>
+
+<!-- Se muestran las gráficas de acuerdo con algunos datos disponibles en la base de datos -->
+
+
+<!-- Importación del archivo para generar gráficas en tiempo real. Para más información https://www.chartjs.org/ -->
+<script type="text/javascript" src="../../resources/js/chart.js"></script>
+
+<?php
+// Se imprime la plantilla del pie enviando el nombre del controlador para la página web.
+Dashboard_Page::footerTemplate('main.js');
+?>
